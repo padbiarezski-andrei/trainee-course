d844efc267b3111e33a72cafacef976bfa413af1
Andrei P <padbiarezski.andrei@gmail.com>
2023-11-03 12:51:08 +0300

    naming

--- basicDataStructures/duplicates/duplicates.go
+++ basicDataStructures/duplicates/duplicates.go
@@ -2,10 +2,10 @@ package duplicates
 
 func removeDuplicates(ss []string) []string {
 	result := make([]string, 0, len(ss))
-	string_set := make(map[string]struct{}, len(ss))
+	stringSet := make(map[string]struct{}, len(ss))
 	for _, s := range ss {
-		if _, ok := string_set[s]; !ok {
-			string_set[s] = struct{}{}
+		if _, ok := stringSet[s]; !ok {
+			stringSet[s] = struct{}{}
 			result = append(result, s)
 		}
 	}
