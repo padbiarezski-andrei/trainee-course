4c1ee33195e200822d24b778897911d83b2934cc
Andrei P <padbiarezski.andrei@gmail.com>
2023-11-08 12:17:53 +0300

    removed task text. added test for shape

--- interfaces/limitReader/interfaces.go
+++ interfaces/limitReader/interfaces.go
@@ -2,13 +2,6 @@ package interfaces
 
 import "io"
 
-/*
-Функция LimitReader из пакета io принимает переменную r типа io .Reader и количество байтов n и возвращает другой объект Reader, который читает из r, но после чтения n сообщает о достижении конца файла.
-Реализуйте его.
-func LimitReader(r io.Reader, n int64) io.Reader
-Для проверки используйте тип bytes.Buffer, который реализует интерфейсы io.Writer, io.Reader
-*/
-
 type myLimitReader struct {
 	r io.Reader
 	n int64
--- interfaces/shapes/shapes_test.go
+++ interfaces/shapes/shapes_test.go
@@ -1,1 +1,29 @@
 package shapes
+
+import (
+	"math"
+	"reflect"
+	"testing"
+)
+
+func TestArea(t *testing.T) {
+	tests := []struct {
+		testName string
+		input    Shape
+		want     float64
+	}{
+		{testName: "baseCircle", input: Circle{R: 10}, want: math.Pi * 10 * 10},
+		{testName: "baseRectangle", input: Rectangle{Width: 10, Height: 20}, want: 10 * 20},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.testName, func(t *testing.T) {
+
+			got := tc.input.Area()
+
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Fatalf("expected: %#v, got: %#v", tc.want, got)
+			}
+		})
+	}
+}
