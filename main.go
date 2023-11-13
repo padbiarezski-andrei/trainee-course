da92ddbf0938f3d3b3860efbced07e45c9cccb73
Andrei P <padbiarezski.andrei@gmail.com>
2023-11-13 12:54:55 +0300

    rewrited. uses only sentinel

--- main.go
+++ main.go
@@ -10,114 +10,30 @@ import (
 	"os"
 )
 
-// ///////////////////////////////////////////////////////////////
-// var ErrInvalidURL = errors.New("invalid URL")
-type ErrInvalidURL struct {
-	url string
-	err error
-}
-
-func newErrInvalidURL(url string, err error) *ErrInvalidURL {
-	return &ErrInvalidURL{
-		url: url,
-		err: err,
-	}
-}
-
-func (e *ErrInvalidURL) Error() string {
-	return fmt.Sprintf("invalid URL %q", e.url)
-}
-
-func (e *ErrInvalidURL) Unwrap() error {
-	return e.err
-}
-
-// ///////////////////////////////////////////////////////////////
-// var ErrConnectionFailed = errors.New("cannot connect")
-type ErrConnectionFailed struct {
-	url string
-	err error
-}
-
-func newErrConnectionFailed(url string, err error) *ErrConnectionFailed {
-	return &ErrConnectionFailed{
-		url: url,
-		err: err,
-	}
-}
-
-func (e *ErrConnectionFailed) Error() string {
-	return fmt.Sprintf("cannot connect to %q", e.url)
-}
-
-func (e *ErrConnectionFailed) Unwrap() error {
-	return e.err
-}
-
-// ///////////////////////////////////////////////////////////////
-// var ErrDownloadFailed = errors.New("cannot download file")
-type ErrDownloadFailed struct {
-	url string
-	err error
-}
-
-func newErrDownloadFailed(url string, err error) *ErrDownloadFailed {
-	return &ErrDownloadFailed{
-		url: url,
-		err: err,
-	}
-}
-
-func (e *ErrDownloadFailed) Error() string {
-	return fmt.Sprintf("cannot download file %q", e.url)
-}
-
-func (e *ErrDownloadFailed) Unwrap() error {
-	return e.err
-}
-
-// ///////////////////////////////////////////////////////////////
-// var ErrFileNotFound = errors.New("not such file")
-type ErrFileNotFound struct {
-	url string
-	err error
-}
-
-func newErrFileNotFound(url string, err error) *ErrFileNotFound {
-	return &ErrFileNotFound{
-		url: url,
-		err: err,
-	}
-}
-
-func (e *ErrFileNotFound) Error() string {
-	return fmt.Sprintf("not such file %q", e.url)
-}
-
-func (e *ErrFileNotFound) Unwrap() error {
-	return e.err
-}
-
-// ///////////////////////////////////////////////////////////////
+var (
+	ErrInvalidURL       = errors.New("invalid URL")
+	ErrConnectionFailed = errors.New("connection failed")
+	ErrDownloadFailed   = errors.New("cannot download file")
+	ErrFileNotFound     = errors.New("file not found")
+)
 
 func downloadFile(fileURL, filename string) (err error) {
 	u, err := url.ParseRequestURI(fileURL) // 1
 	if err != nil {
-		return newErrInvalidURL(fileURL, err)
+		return errors.Join(ErrInvalidURL, err)
 	}
 
 	resp, err := http.Get(u.String())
-	var urlErr *url.Error        // ?
-	if errors.As(err, &urlErr) { // 2
-		return newErrConnectionFailed(fileURL, err)
+	if err != nil { // 2
+		return errors.Join(ErrConnectionFailed, err)
 	}
 
 	if resp.StatusCode == http.StatusInternalServerError { // 3
-		return newErrDownloadFailed(fileURL, err)
+		return ErrDownloadFailed
 	}
 
 	if resp.StatusCode == http.StatusNotFound { // 4
-		return newErrFileNotFound(fileURL, err)
+		return ErrFileNotFound
 	}
 
 	outFile, err := os.Create(filename)
@@ -145,23 +61,19 @@ func main() {
 
 	err := downloadFile(*url, *path)
 	if err != nil {
-		var errInvalidURL *ErrInvalidURL
-		var errConnectionFailed *ErrConnectionFailed
-		var errDownloadFailed *ErrDownloadFailed
-		var errNotFOund *ErrFileNotFound
 
 		switch {
-		case errors.As(err, &errInvalidURL):
-			fmt.Printf("Invalide URL. Please check the URL correctness. err:%s\n", errInvalidURL)
+		case errors.Is(err, ErrInvalidURL):
+			fmt.Printf("Invalide URL %q. Please check the URL correctness. err:%s\n", *url, err)
 
-		case errors.As(err, &errConnectionFailed):
-			fmt.Printf("Cannot connect to %q. Please check your connection settings or try to again later. err:%s\n", *url, errConnectionFailed)
+		case errors.Is(err, ErrConnectionFailed):
+			fmt.Printf("Cannot connect to %q. Please check your connection settings or try to again later. err:%s\n", *url, err)
 
-		case errors.As(err, &errDownloadFailed):
-			fmt.Printf("Error occured while downloading file %q. Please check if file is available. err:%s\n", *url, errDownloadFailed)
+		case errors.Is(err, ErrDownloadFailed):
+			fmt.Printf("Error occured while downloading file %q. Please check if file is available. err:%s\n", *url, err)
 
-		case errors.As(err, &errNotFOund):
-			fmt.Printf("No such file found. Please check if the file available or URL %q is correct.\n", *url)
+		case errors.Is(err, ErrFileNotFound):
+			fmt.Printf("No such file found. Please check if the file available or URL %q is correct. err:%s\n", *url, err)
 
 		default:
 			fmt.Println("error occurred: ", err)
