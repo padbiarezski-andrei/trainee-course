f35e34fca51ace7ef0eb4f881ddb30989324d04a
Andrei P <padbiarezski.andrei@gmail.com>
2023-11-14 21:52:23 +0300

    squashed test

--- main.go
+++ main.go
@@ -10,7 +10,22 @@ import (
 	"time"
 )
 
-var r = rand.New(rand.NewSource(time.Now().Unix()))
+// go run . --K=1000 --N=50 --M=10
+// Done! Total cake 71 out of 1000 in 5.716077123s, delay was 3s
+
+// go run . --K=1000 --N=8 --M=5
+// Done! Total cake 28 out of 1000 in 4.993860512s, delay was 3s
+
+// go run . --K=1000 --N=250 --M=125
+// Done! Total cake 849 out of 1000 in 7.796713981s, delay was 3s
+
+// go run . --K=1000 --N=125 --M=250
+// Done! Total cake 886 out of 1000 in 6.817758505s, delay was 3s
+
+var r = struct {
+	m   *sync.Mutex
+	src *rand.Rand
+}{m: &sync.Mutex{}, src: rand.New(rand.NewSource(time.Now().Unix()))}
 
 type cake struct {
 	id       uint64
@@ -26,8 +41,10 @@ func (c cake) String() string {
 }
 
 func getRandDuration(i uint64) time.Duration {
+	r.m.Lock()
+	defer r.m.Unlock()
 	return time.Microsecond*time.Duration(i) +
-		time.Microsecond*time.Duration(int64(float64(r.Intn(1000))*r.Float64())) +
+		time.Nanosecond*time.Duration(r.src.Int63n(1000000)) +
 		time.Microsecond
 }
 
@@ -42,6 +59,8 @@ func bake(ctx context.Context, wg *sync.WaitGroup, backedCakeOutCh chan<- cake, 
 				time.Sleep(T1)
 
 				backedCakeOutCh <- cake{id: id, BakedBy: i, BakeTime: T1}
+			} else {
+				return
 			}
 		case <-ctx.Done():
 			return
@@ -62,6 +81,8 @@ func pack(ctx context.Context, wg *sync.WaitGroup, packedCakeOutCh chan<- cake, 
 				c.PackTime = T2
 
 				packedCakeOutCh <- c
+			} else {
+				return
 			}
 		case <-ctx.Done():
 			return
@@ -70,63 +91,42 @@ func pack(ctx context.Context, wg *sync.WaitGroup, packedCakeOutCh chan<- cake, 
 }
 
 func main() {
-<<<<<<< HEAD
 	totalCake := flag.Uint64("K", 0, "total cakes required")
 	N := flag.Uint64("N", 0, "total furnaces")
 	M := flag.Uint64("M", 0, "total packers")
 
 	flag.Parse()
 
-	delay := 5 * time.Second
+	// var q uint64 = 1000
+	// var a uint64 = 50
+	// var z uint64 = 10
+	// var totalCake *uint64 = &q
+	// var N *uint64 = &a
+	// var M *uint64 = &z
+
+	delay := 3 * time.Second
 	start := time.Now()
 
 	currentCakeCh := make(chan uint64, runtime.NumCPU())
 	bakedCakeCh := make(chan cake, runtime.NumCPU())
 	packedCakeCh := make(chan cake, runtime.NumCPU())
 
-=======
-	// totalCake := flag.Uint64("K", 0, "total cakes required")
-	// N := flag.Uint64("N", 0, "total furnaces")
-	// M := flag.Uint64("M", 0, "total packers")
-
-	start := time.Now()
-	var totalCake uint64 = 1000
-	var N uint64 = 1
-	var M uint64 = 1
-
-	delay := 5 * time.Second
-
-	flag.Parse()
-
-	currentCakeCh := make(chan uint64, runtime.NumCPU())
-	bakedCakeCh := make(chan cake, runtime.NumCPU())
-	packedCakeCh := make(chan cake, runtime.NumCPU())
-
->>>>>>> dfd8ab85aae3bf75030bebcffa21bdc20d0fe3a7
 	var wgFurnaces sync.WaitGroup
 	var wgPackers sync.WaitGroup
 
 	ctx := context.Background()
 	ctx, cancel := context.WithTimeout(ctx, delay)
 	defer cancel()
-	time.AfterFunc(5*time.Second, cancel)
 
 	go func(ctx context.Context, totalCakesTo uint64, outCh chan<- uint64) {
 		for i := uint64(0); i < totalCakesTo; i++ {
 			outCh <- i
 		}
 		close(outCh)
-<<<<<<< HEAD
 	}(ctx, *totalCake, currentCakeCh)
 
 	go func() {
 		for i := uint64(0); i < *N; i++ {
-=======
-	}(ctx, totalCake, currentCakeCh)
-
-	go func() {
-		for i := uint64(0); i < N; i++ {
->>>>>>> dfd8ab85aae3bf75030bebcffa21bdc20d0fe3a7
 			wgFurnaces.Add(1)
 			go bake(ctx, &wgFurnaces, bakedCakeCh, currentCakeCh, i)
 		}
@@ -136,11 +136,7 @@ func main() {
 	}()
 
 	go func() {
-<<<<<<< HEAD
 		for i := uint64(0); i < *M; i++ {
-=======
-		for i := uint64(0); i < M; i++ {
->>>>>>> dfd8ab85aae3bf75030bebcffa21bdc20d0fe3a7
 			wgPackers.Add(1)
 			go pack(ctx, &wgPackers, packedCakeCh, bakedCakeCh, i)
 		}
@@ -162,5 +158,5 @@ func main() {
 		totalPackedBakedCake++
 		fmt.Println(totalPackedBakedCake, c)
 	}
-	fmt.Printf("Done! Total cake %v out of %v in %v, delay was %v", totalPackedBakedCake, totalCake, time.Since(start), delay)
+	fmt.Printf("Done! Total cake %v out of %v in %v, delay was %v\n", totalPackedBakedCake, *totalCake, time.Since(start), delay)
 }
