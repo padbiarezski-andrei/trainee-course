31ceda2683356aa141e56bfcf81c829b21761924
Andrei P <padbiarezski.andrei@gmail.com>
2023-11-14 21:01:15 +0300

    cleaned up

--- main.go
+++ main.go
@@ -10,34 +10,6 @@ import (
 	"time"
 )
 
-/*
-С помощью горутин и каналов смоделировать работу пекарни:
-
-Пекарня состоит из 2+1 этапов: торт выпекается, торт запаковывается, все торты смотрятся. Связь между этапами происходит через каналы,
-этапы выполняются с помощью горутин. Торт представляет из себя объект с полями типа int: BakedBy, BakeTime, PackedBy, PackTime
-
-Программа должна работать следующим образом:
-
-Запускается N горутин, каждая из которых за время T1 = i +-t1 (i - номер рутины, t1 - выбранный вами параметр) создаёт объект тортика,
-заполняет поля BackedBy=i, BakeTime=T1 и отправляет его в канал
-
-Существует пул из M горутин. когда в канал приходят торитики с предыдущего этапа,
-свободная рутина из пула начинает его упаковывать,
-то есть за время T2 = j+-t2 (j - номер рутины из пула, t2 - выбранный вами параметр, причём t2>=t1)
-выставляет тортику параметры PackedBy=j, PackTime = T2 и отправляет в канал
-
-Мы ждём пока придут все тортики, или мы получим сигнал о завершении работы(тогда дожидаемся завершения работы текущих рутин),
-после чего просто выводим тортики в порядке, в котором они пришли к нам. При полном отрабатывании программы должно быть K тортиков.
-
-Для данной задачи можно использовать различные наборы параметров, однако рекоммендуется попробовать следующие комбинации:
-
-K = 10_000, N = 1, M = 1
-
-K = 10_000, N = 8, M = 5
-
-K = 10_000, N = 100, M = 50
-*/
-
 var r = rand.New(rand.NewSource(time.Now().Unix()))
 
 type cake struct {
@@ -98,58 +70,52 @@ func pack(ctx context.Context, wg *sync.WaitGroup, packedCakeOutCh chan<- cake, 
 }
 
 func main() {
-	// totalCake := flag.Uint64("K", 0, "total cakes for baking")
-	// N := flag.Int("N", 0, "total furnaces")
-	// M := flag.Int("M", 0, "total packers")
-	// c := cake{1, time.Duration(1 * time.Second), 1, time.Microsecond * 2}
-	// fmt.Println(c)
-	// return
-	start := time.Now()
-	var totalCake uint64 = 1000
-	var N uint64 = 1
-	var M uint64 = 1
-	delay := 5 * time.Second
+	totalCake := flag.Uint64("K", 0, "total cakes required")
+	N := flag.Uint64("N", 0, "total furnaces")
+	M := flag.Uint64("M", 0, "total packers")
 
 	flag.Parse()
 
-	currentCakeProducerCh := make(chan uint64, runtime.NumCPU())
-	bakedCakeProducerCh := make(chan cake, runtime.NumCPU())
-	packedCakeProducerCh := make(chan cake, runtime.NumCPU())
+	delay := 5 * time.Second
+	start := time.Now()
 
-	var wgFurnace sync.WaitGroup
-	var wgPacker sync.WaitGroup
+	currentCakeCh := make(chan uint64, runtime.NumCPU())
+	bakedCakeCh := make(chan cake, runtime.NumCPU())
+	packedCakeCh := make(chan cake, runtime.NumCPU())
+
+	var wgFurnaces sync.WaitGroup
+	var wgPackers sync.WaitGroup
 
 	ctx := context.Background()
 	ctx, cancel := context.WithTimeout(ctx, delay)
+	defer cancel()
 	time.AfterFunc(5*time.Second, cancel)
 
 	go func(ctx context.Context, totalCakesTo uint64, outCh chan<- uint64) {
-		i := uint64(0)
-		for ; i < totalCakesTo; i++ {
+		for i := uint64(0); i < totalCakesTo; i++ {
 			outCh <- i
 		}
-		fmt.Println("all orders passed. Total:", i)
 		close(outCh)
-	}(ctx, totalCake, currentCakeProducerCh)
+	}(ctx, *totalCake, currentCakeCh)
 
 	go func() {
-		for i := uint64(0); i < N; i++ {
-			wgFurnace.Add(1)
-			go bake(ctx, &wgFurnace, bakedCakeProducerCh, currentCakeProducerCh, i)
+		for i := uint64(0); i < *N; i++ {
+			wgFurnaces.Add(1)
+			go bake(ctx, &wgFurnaces, bakedCakeCh, currentCakeCh, i)
 		}
 
-		wgFurnace.Wait()
-		close(bakedCakeProducerCh)
+		wgFurnaces.Wait()
+		close(bakedCakeCh)
 	}()
 
 	go func() {
-		for i := uint64(0); i < M; i++ {
-			wgPacker.Add(1)
-			go pack(ctx, &wgPacker, packedCakeProducerCh, bakedCakeProducerCh, i)
+		for i := uint64(0); i < *M; i++ {
+			wgPackers.Add(1)
+			go pack(ctx, &wgPackers, packedCakeCh, bakedCakeCh, i)
 		}
 
-		wgPacker.Wait()
-		close(packedCakeProducerCh)
+		wgPackers.Wait()
+		close(packedCakeCh)
 	}()
 
 	go func(cf context.CancelFunc) {
@@ -161,7 +127,7 @@ func main() {
 	}(cancel)
 
 	totalPackedBakedCake := uint64(0)
-	for c := range packedCakeProducerCh {
+	for c := range packedCakeCh {
 		totalPackedBakedCake++
 		fmt.Println(totalPackedBakedCake, c)
 	}
